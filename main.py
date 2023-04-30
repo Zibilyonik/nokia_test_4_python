038edb378389eb2dbf16844f4fc80f7a337941bb
Onur Surmeli <onatsurmeli@gmail.com>
2023-04-30 18:31:17 +0200

    initial commit, completed assignment

---
+++ main.py
@@ -0,0 +1,15 @@
+import re
+
+def parse_config(config_file):
+    with open(config_file) as f:
+        config = f.read()
+    config = re.sub(r'//.*', '', config)
+    config_dict = {}
+    for line in config.split('\n'):
+        if line.startswith('#'):
+            continue
+        if line.strip() == '':
+            continue
+        key, value = line.split('=')
+        config_dict[key.strip()] = value.strip()
+    return config_dict
